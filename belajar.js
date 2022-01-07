53e79b340efbdd71264a7f1c881688b8262ff5a7
kangjanur <nurjaman230897@gmail.com>
2022-01-07 22:03:57 +0700

    Fixbug

--- belajar.js
+++ belajar.js
@@ -1,1 +1,9 @@
+/ POST /login
 
+if (req.body.email && req.body.password) {
+
+} else {
+    var err = new Error('Email and password are required.');
+    err.status = 401;
+    return next(err);
+}
